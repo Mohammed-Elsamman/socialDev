645a89ee1292b0ad22ff3270eefd3fcfdb2da9b9
samman <monem7242@gmail.com>
2019-08-30 21:08:06 +0200

    create route for like post and route to comment on post and route to remove comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const passport = require("passport");
 const Post = require("../../models/Post");
 const User = require("../../models/User");
-const validationPostInpot = require("../../validation/post")
+const validationPostInpot = require("../../validation/post");
+const validationCommentInpot = require("../../validation/comment");
 
 router.get('/test', (req, res) => res.json({msg: "posts is works"}));
 
@@ -80,6 +81,57 @@ router.get('/:id',
 );
 
 // @route   DELETE api/post/:id
+// @desc    ADD like for post
+// @access  private
+router.post('/like/:id',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        Post.findById(req.params.id)
+            .then(post => {
+                if (!post) {
+                    res.status(404).json({nopst: "there is no post"})
+                }
+                if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+                    res.status(404).json({alreadyliked: "User already liked this post"})
+                }
+
+                post.likes.unshift({user: req.user.id});
+                post.save().then(post => res.json(post))
+            })
+            .catch(err => res.status(404).json({postnotfound: "No Post Found"}))
+    }
+);
+
+
+// @route   DELETE api/comment/:id
+// @desc    ADD comment for post
+// @access  private
+router.post('/comment/:id',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        const {errors, isValid} = validationCommentInpot(req.body);
+        if (!isValid) {
+            return res.status(404).json({errors})
+        }
+        Post.findById(req.params.id)
+            .then(post => {
+                if (!post) {
+                    res.status(404).json({nopst: "there is no post"})
+                }
+                const newComment = {
+                    user: req.user.id,
+                    text: req.body.text,
+                    name: req.body.name,
+                    avatar: req.body.avatar
+                }
+                post.comments.unshift(newComment);
+                post.save().then(post => res.json(post))
+            })
+            .catch(err => res.status(404).json({postnotfound: "No Post Found"}))
+    }
+);
+
+// @route   POST api/post/like/:id
 // @desc    DELETE post
 // @access  private
 router.delete('/:id',
@@ -95,6 +147,35 @@ router.delete('/:id',
                     res.status(404).json({noauthorized: "user no authorized"})
                 }
                 post.remove().then(() => res.json({success: true}))
+            })
+            .catch(err => res.status(404).json(err))
+    }
+);
+
+// @route   POST api/post/comment/:id/:cid
+// @desc    DELETE comment on post
+// @access  private
+router.delete('/comment/:id/:cid',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        console.log(req.params.id);
+        Post.findById(req.params.id)
+            .then(post => {
+                if (!post) {
+                    res.status(404).json({nopst: "there is no post"})
+                }
+                comments = post.comments.filter(comment => comment.id === req.params.cid);
+                console.log(comments);
+                if(comments.length === 0){
+                    res.status(404).json({nocomment: "there is no comment to delete"})
+                }
+                if(comments[0].user.toString() !== req.user.id || comments[0].user.toString() !== post.user.toString()){
+                    res.status(404).json({noauthorized: "user no authorized"})
+                }
+                comment = post.comments.map(cmnt => cmnt.id ).indexOf(req.params.cid)
+                console.log(comment);
+                post.comments.splice(comment,1);
+                post.save().then(() => res.json(post))
             })
             .catch(err => res.status(404).json(err))
     }
---
+++ validation/comment.js
@@ -0,0 +1,18 @@
+const validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = validateLoginInput = data => {
+    let errors = {};
+
+    data.text = !isEmpty(data.text) ? data.text : "";
+
+    if (validator.isEmpty(data.text)) {
+        errors.text = "text is required"
+    }
+
+
+    return {
+        errors,
+        isValid: isEmpty(errors),
+    }
+};
