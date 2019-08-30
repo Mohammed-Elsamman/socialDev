23ae6ed2e39b4bd61cb69b9869e928d65eaa01b2
samman <monem7242@gmail.com>
2019-08-30 20:06:42 +0200

    create route for removing post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const post = require("../../models/Post");
-const user = require("../../models/User");
+const Post = require("../../models/Post");
+const User = require("../../models/User");
 const validationPostInpot = require("../../validation/post")
 
 router.get('/test', (req, res) => res.json({msg: "posts is works"}));
@@ -18,7 +18,7 @@ router.post('/',
         if (!isValid) {
             return res.status(404).json({errors})
         }
-        const newPost = new post({
+        const newPost = new Post({
             text: req.body.text,
             name: req.body.name,
             avatar: req.body.avatar,
@@ -32,16 +32,13 @@ router.post('/',
 // @desc    get all post
 // @access  private
 router.get('/',
-    passport.authenticate('jwt', {session: false}),
     (req, res) => {
-        const errors = {}
-        post.find()
-            .then(posts => {
+        Post.find()
+            .then(post => {
                 if (!post) {
-                    errors.noPosts = "there is no posts"
-                    res.status(404).json(errors)
+                    res.status(404).json({noPosts: "there is no posts"})
                 }
-                res.json(posts)
+                res.json(post)
             })
             .catch(err => res.status(404).json(err))
     }
@@ -53,14 +50,12 @@ router.get('/',
 router.get('/my_post',
     passport.authenticate('jwt', {session: false}),
     (req, res) => {
-        const errors = {}
-        post.find({user: req.user.id})
-            .then(posts => {
+        Post.find({user: req.user.id})
+            .then(post => {
                 if (!post) {
-                    errors.noPosts = "there is no posts for that user";
-                    res.status(404).json(errors)
+                    res.status(404).json({nopst: "there is no post for that user"})
                 }
-                res.json(posts)
+                res.json(post)
             })
             .catch(err => res.status(404).json(err))
     }
@@ -73,14 +68,33 @@ router.get('/my_post',
 router.get('/:id',
     passport.authenticate('jwt', {session: false}),
     (req, res) => {
-        const errors = {}
-        post.find({_id: req.params.id})
-            .then(posts => {
+        Post.find({_id: req.params.id})
+            .then(post => {
                 if (!post) {
-                    errors.noPosts = "there is no post";
-                    res.status(404).json(errors)
+                    res.status(404).json({nopst: "there is no post"})
                 }
-                res.json(posts)
+                res.json(post)
+            })
+            .catch(err => res.status(404).json(err))
+    }
+);
+
+// @route   DELETE api/post/:id
+// @desc    DELETE post
+// @access  private
+router.delete('/:id',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        console.log(req.params.id);
+        Post.findById(req.params.id)
+            .then(post => {
+                if (!post) {
+                    res.status(404).json({nopst: "there is no post"})
+                }
+                if (post.user.toString() !== req.user.id) {
+                    res.status(404).json({noauthorized: "user no authorized"})
+                }
+                post.remove().then(() => res.json({success: true}))
             })
             .catch(err => res.status(404).json(err))
     }
