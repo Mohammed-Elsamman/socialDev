ffb0a0e3c672c23ed96c31216dda449dcfd7f7a5
samman <monem7242@gmail.com>
2019-08-31 05:42:53 +0200

    create navbar, footer and landing compomemts

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Navbar from "../src/components/layout/Navbar"
+import Footer from "../src/components/layout/Footer"
+import Landing from "../src/components/layout/Landing"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>my react app</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Navbar/>
+                <Landing/>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
