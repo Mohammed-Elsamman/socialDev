74f5de43c38bc91280f3fdcb6763849b6c4538ba
samman <monem7242@gmail.com>
2019-10-22 20:21:31 +0200

    create group model

---
+++ models/Group.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//create Post Schema
+const groupSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+    },
+    name: {
+        type: String,
+        require: true
+    },
+    Managers: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users"
+            }
+        }
+    ],
+    resquests: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users"
+            }
+        }
+    ],
+    members: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: "users"
+            }
+        }
+    ],
+    description: {
+        type: String,
+        require: true
+    },
+    interestedin: {
+        type: Array,
+        require: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+module.exports = Post = mongoose.model("group", groupSchema);
